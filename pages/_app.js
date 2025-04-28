214d20ca915fa76cc88bd172d7a55a6cc07dbfbd
YordsBoy <rofam33@gmail.com>
2025-04-28 04:40:54 +0000

    chore: update import to styles/global.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import "../public/output.css"; 
+// pages/_app.js
+import "../styles/globals.css";
 import { UserTierProvider } from "@/components/UserTierContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <UserTierProvider>
+      <Component {...pageProps} />
+    </UserTierProvider>
+  );
 }
